91067937e7d9d124d65d40afb752789157d636f2
Valeria Rodriguez Fontenla <valrodfon@hotmail.com>
2021-08-22 16:42:30 -0300

    ¨categorias¨

---
+++ assetscategories/categorias.js
@@ -0,0 +1,5 @@
+var formAddCategory = document.getElementById('form-add-category');
+formAddCategory.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('funciona');
+});
---
+++ assetscategories/categorias.ts
@@ -0,0 +1,33 @@
+type LocalStorage = {
+        Categories?: Category[]
+
+}
+
+type Category = {
+        id: number,
+        title: string,
+        slug: string
+    }
+
+const formAddCategory = document.getElementById('form-add-category');
+const storage:LocalStorage = {}
+
+const createCategory = (e) => {
+     e.preventDefault();
+
+     const form = e.target;
+     const newCategoryName: string = form.name.value;
+
+     const newCategory: Category = {
+         id: 1,
+         name: newCategoryName,
+         slug: slugify(newCategoryName)
+
+     }
+     console.log(newCategory);
+     
+} 
+formAddCategory.addEventListener('submit',createCategory) 
+
+
+
